d7232b01f9a78e9aa42525fce68602b9c9080709
bssrdf <merlintiger@hotmail.com>
2020-08-02 18:25:57 -0400

    work on a python version

---
+++ code_samples/markov.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+import sys
+
+NPREF = 2
+
+
+
+def build(infile):
+    hashtab = defaultdict(list) 
+    prefix = ['\n']*NPREF
+    with file as open(infile):
+       for line in file:
+           
+
+
+def run():
+    hashtab = buid(sys.argv[1])
+
+
+if __name__ == "__main__":
+    run()
