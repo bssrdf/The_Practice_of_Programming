351518b967ee94b5b917c22f6e2b5c53821ff19b
bssrdf <merlintiger@hotmail.com>
2020-08-03 21:39:51 -0400

    markov using python done

--- code_samples/markov.py
+++ code_samples/markov.py
@@ -1,20 +1,36 @@
-from collections import defaultdict
 import sys
+
+from collections import defaultdict
+from random import choice
 
 NPREF = 2
 
 
+def shift1(li, s):
+    for i in range(0, len(li)-1):
+        li[i] = li[i+1]   
+    li[len(li)-1] = s
+    
 
 def build(infile):
     hashtab = defaultdict(list) 
     prefix = ['\n']*NPREF
-    with file as open(infile):
+    with open(infile) as file:
        for line in file:
-           
-
+           for word in line.split():
+               hashtab[''.join(prefix)].append(word)
+               shift1(prefix, word)          
+          
+    return hashtab
 
 def run():
-    hashtab = buid(sys.argv[1])
+    hashtab = build(sys.argv[1])    
+    prefix = ['\n']*NPREF
+    for i in range(1000):
+        surfix = choice(hashtab[''.join(prefix)])        
+        shift1(prefix, surfix)
+        print(surfix, end=' ')
+
 
 
 if __name__ == "__main__":
